88014ab842efd89c50c740ef3de3738b6cbab07e
GustavoAquino36 <gustavoaquino357@gmail.com>
2023-02-03 12:01:22 -0300

    fix/simplificacaoParaFuturaAtualizacao

--- ApiTreatment.py
+++ ApiTreatment.py
@@ -1,14 +1,11 @@
 import requests
 
 def GetApi(api):
-    response = requests.get(f"{api}")
-    if response.status_code == 200:
-        print(response.json())
-        virgula = str(response.json()).split(',')
-        for i in virgula:
-            print(i)
-    else:
-        print(f"erro: {response.status_code}")
+  response = requests.get(f"{api}")
+  if response.status_code == 200:
+    print(response.json())
+  else:
+    print(f"erro: {response.status_code}")
 
 ApiUrl = input(f'Api URL: ')
 
